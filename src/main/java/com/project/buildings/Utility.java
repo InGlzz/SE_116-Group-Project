91266695dfdeba79ff33a46c41d15800fec720c4
InGlzz <hamzagedik369077@gmail.com>
2026-05-07 23:16:15 +0300

    Small name change

--- src/main/java/com/project/buildings/Utility.java
+++ src/main/java/com/project/buildings/Utility.java
@@ -1,12 +1,12 @@
 package com.project.buildings;
 
 public abstract class Utility extends Cell {
-	private int capacity;
+	private int maximumCapacity;
 
 	public Utility(int x, int y, char mapInput) {
 		super(x, y, mapInput);
-		this.capacity = 100;
+		this.maximumCapacity = 100;
 	}
 
-	public int getCapacity() {return capacity;}
+	public int getCapacity() {return maximumCapacity;}
 }
