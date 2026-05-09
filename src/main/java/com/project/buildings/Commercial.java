b28da9a268e45580b6f1ac73ef1a226051a869e6
oguzhanarag <oguzhanarag@gmail.com>
2026-05-10 02:59:05 +0300

    implement trade logic, goods consume, and lifestyle generation for Commerical

--- src/main/java/com/project/buildings/Commercial.java
+++ src/main/java/com/project/buildings/Commercial.java
@@ -1,3 +1,17 @@
 package com.project.buildings;
 public class Commercial extends Zone {
+    public Commercial(int x, int y, char mapInput){
+        super(x,y,mapInput);
+    }
+    @Override
+    public void doTick(){
+        if(getElectricity() > 0 && getGoods() > 10){
+            setGoods(getGoods()-10);//it sells goods that's why it decreases
+            if(getLevel()<3){
+                setLevel(getLevel()+1);//if there are goods n electricity; it can level up
+            }
+            setLifestyle(getLifestyle() + (getLevel() * 5));//As it make sales it bring a new lifestyle to the city
+        }else{
+        }
+    }
 }
