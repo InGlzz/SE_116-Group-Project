b8ad8b1988710b4fb84d48bee1847108c0106b27
InGlzz <hamzagedik369077@gmail.com>
2026-05-16 18:23:30 +0300

    Finished going to do some tests.

--- src/main/java/com/project/reader/Reader.java
+++ src/main/java/com/project/reader/Reader.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Reader {
-	int row;
-	int col;
+	int x;
+	int y;
 	char[][] building;
 	String fileName;
 	List<String> lines;
@@ -26,9 +26,26 @@ public class Reader {
 					lines.add(line);
 				}
 			}
-
 		} catch (IOException e) {
 			System.err.println("File Error! " + this.fileName);
 		}
+
+		//calculate x and y values
+		this.y = lines.size();
+		this.x = lines.get(0).length();
+		this.building = new char[this.y][this.x];
+
+		//set the Characters in a 2d array.
+		for(int i = 0; i < this.y; y++) {
+			String line = lines.get(i);
+			for(int j = 0; j < this.x; x++) {
+				this.building[y][x] = line.charAt(x);
+			}
+		}
 	}
+
+	//getter methods
+	public int getX() {return this.x;}
+	public int getY() {return this.y;}
+	public char[][] getBuilding() {return this.building;}
 }
