14fa24f489a3ba62e098d06357bcc9cd118c99df
oguzhanarag <oguzhanarag@gmail.com>
2026-05-16 18:43:05 +0300

    Implemented level-based progression for House, Zone and Commerical Overrided doTick method for each Zone

--- src/main/java/com/project/buildings/Commercial.java
+++ src/main/java/com/project/buildings/Commercial.java
@@ -1,17 +1,40 @@
 package com.project.buildings;
+
 public class Commercial extends Zone {
     public Commercial(int x, int y, char mapInput){
-        super(x,y,mapInput);
+        super(x, y, mapInput);
     }
+
     @Override
     public void doTick(){
-        if(getElectricity() > 0 && getGoods() > 10){
-            setGoods(getGoods()-10);//it sells goods that's why it decreases
-            if(getLevel()<3){
-                setLevel(getLevel()+1);//if there are goods n electricity; it can level up
+        int currentLevel = getLevel();
+
+        // if there are population,goods,elec.,water and internet it can level up to level 1
+        if (getPopulation() > 0 && getGoods() > 10 && getElectricity() > 0 && getWater() > 0 && getInternet() > 0) {
+
+            //in commercial, it sells goods so goods stock must be decreases
+            setGoods(getGoods() - 10);
+            if (currentLevel == 0) {
+                setLevel(1);
             }
-            setLifestyle(getLifestyle() + (getLevel() * 5));//As it make sales it bring a new lifestyle to the city
-        }else{
+            // if it is on level 1 and it has level 2 it can level up to level 2
+            else if (currentLevel == 1) {
+                if (getHasSecurity() == true) {
+                    setLevel(2);
+                }
+            }
+
+            // if it is on level 2 and it has excees population(like 25) and excees goods(like 30) it can level up to level 3
+            else if (currentLevel == 2) {
+                if (getPopulation() > 25 && getGoods() > 30) {
+                    setLevel(3);
+                }
+            }
+            // lifestyle is proportional to level so if it level up, lifestyle is also life style is increases
+            setLifestyle(getLifestyle() + (getLevel() * 5));
+
+        } else {
+            //if it can access to essential things trade is end
         }
     }
 }
--- src/main/java/com/project/buildings/House.java
+++ src/main/java/com/project/buildings/House.java
@@ -1,22 +1,35 @@
 package com.project.buildings;
 
 public class House extends Zone {
-    public House(int x,int y, char mapInput){
-        super(x,y,mapInput);
+    public House(int x, int y, char mapInput){
+        super(x, y, mapInput);
     }
+
     @Override
     public void doTick(){
-        if(getElectricity() > 0 && getWater() > 0){ //for growth; electricity n water are essential
-            if (getLevel() < 3) {
-                setLevel(getLevel() + 1); // if it is not level 3 it can level up
+        int currentLevel = getLevel();
+
+        if (getElectricity() > 0 && getWater() > 0 && getInternet() > 0) {//if there are electrictiy, water and internet it can level up to level 1
+            if (currentLevel == 0) {
+                setLevel(1);
             }
-            setPopulation(getLevel()*10);//population is increases 10 people for each level
-            if(getInternet()>0){
-                setLifestyle(getLifestyle()+5);
+            else if (currentLevel == 1) {
+                if (getHasSecurity() == true && getHasHealth() == true && getHasEducation() == true) {//if there are security, health and education it can level up to level 2
+                    setLevel(2);
+                }
             }
-        }else{
-            if(getPopulation()>0){
-                setPopulation(getPopulation()-1);//if there are no water n electricity population might be decrease
+            //if house get lifestyle it can level up to level 3
+            else if (currentLevel == 2) {
+                if (getLifestyle() > 0) {
+                    setLevel(3);
+                }
+            }
+            //population generating
+            setPopulation(getLevel() * 10);
+        } else {
+            // if there are no electric health and education while there are population, the populaiton will decreasse
+            if (getPopulation() > 0) {
+                setPopulation(getPopulation() - 1);
             }
         }
     }
