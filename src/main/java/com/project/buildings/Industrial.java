f68e4b1326c3a0eaa5b071c6825cde8a00811e6e
oguzhanarag <oguzhanarag@gmail.com>
2026-05-24 16:25:32 +0300

    crate output logic for industrial

--- src/main/java/com/project/buildings/Industrial.java
+++ src/main/java/com/project/buildings/Industrial.java
@@ -11,6 +11,17 @@ public class Industrial extends Zone {
 
         // if there are population, electricity and water it can level up to level 1
         if (getPopulation() > 0 && getElectricity() > 0 && getWater() > 0) {
+
+            //calculate the m value
+            int m = getPopulation();
+            if (getElectricity() < m) {
+                m = getElectricity();
+            }
+            if (getWater() < m) {
+                m = getWater();
+            }
+
+            // Level up control
             if (currentLevel == 0) {
                 setLevel(1);
             }
@@ -21,18 +32,23 @@ public class Industrial extends Zone {
                     setLevel(2);
                 }
             }
-            //if it is on level 2 and it has excees population like 25 it can level up to level 3
-            else if (currentLevel == 2) {
+            else if (currentLevel == 2) {     //if it is on level 2 and it has excees population like 25 it can level up to level 3
                 if (getPopulation() > 25) {
                     setLevel(3);
                 }
             }
-            // goods is proportional to level so if it level up, goods are also increase
-            setGoods(getLevel() * 10);
-
+            // logic of setOutput
+            if (getLevel() == 1) {
+                setOutput(m);
+            } else if (getLevel() == 2) {
+                setOutput(m * 2);
+            } else if (getLevel() == 3) {
+                setOutput((m * 2) + getPopulation());
+            }
         } else {
-            // if there are no population and essential things can't generate goods
-            setGoods(0);
+            // if basic needs not met zone collapses instantly
+            setOutput(0);
+            setLevel(0);
         }
     }
 }
