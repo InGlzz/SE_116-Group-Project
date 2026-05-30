22b4b6c34f2650640ab2767beee94cfc0ba28266
oguzhanarag <oguzhanarag@gmail.com>
2026-05-30 23:58:13 +0300

    EmptCell and Road classes

--- src/main/java/com/project/buildings/EmptyCell.java
+++ src/main/java/com/project/buildings/EmptyCell.java
@@ -1,4 +1,10 @@
 package com.project.buildings;
 
-public class EmptyCell {
+public class EmptyCell extends Cell {
+    public EmptyCell(int x, int y, char mapInput){
+        super(x, y, mapInput);
+    }
+    @Override
+    public void doTick(){// Must be empty because there is no internal logic needed
+    }
 }
