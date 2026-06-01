c6ca24ec0f642edb0eb46f3fbaaaed3d3cd5c369
atakangoek <49897941+atakangoek@users.noreply.github.com>
2026-06-01 21:53:37 +0300

    Added output for industrial level ups/downs and when they generate output

--- src/main/java/com/project/buildings/Industrial.java
+++ src/main/java/com/project/buildings/Industrial.java
@@ -8,6 +8,7 @@ public class Industrial extends Zone {
     @Override
     public void doTick(){
         int currentLevel = getLevel();
+        int previousLevel = getLevel();
 
         // if there are population, electricity and water it can level up to level 1
         if (getPopulation() > 0 && getElectricity() > 0 && getWater() > 0) {
@@ -44,6 +45,14 @@ public class Industrial extends Zone {
             // if basic needs not met zone collapses instantly
             setOutput(0);
             setLevel(0);
+        }
+
+        System.out.println("Industrial at (" + getX() + "," + getY() + ") generated " + getOutput() + " goods");
+
+        if (getLevel() > previousLevel) {
+            System.out.println("Industrial at (" + getX() + "," + getY() + ") levels up from " + previousLevel + " to " + getLevel());
+        } else if (getLevel() < previousLevel) {
+            System.out.println("Industrial at (" + getX() + "," + getY() + ") levels down from " + previousLevel + " to " + getLevel());
         }
     }
 }
