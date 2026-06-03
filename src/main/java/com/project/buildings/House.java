92446b88a586b1399d263ddb0b3746c08e22bc47
oguzhanarag <oguzhanarag@gmail.com>
2026-06-03 22:43:54 +0300

    implement dynamic level progression and output logic for core zones - Added incremental level-up and level-down mechanics in House, Commercial, and Industrial classes. - Integrated infrastructure constraints (electricity, water, internet, security) for building evolution. - Implemented class-specific resource output algorithms based on current building levels. - Added simulation state log prints for tracking level shifts and lifestyle production.

--- src/main/java/com/project/buildings/House.java
+++ src/main/java/com/project/buildings/House.java
@@ -10,42 +10,46 @@ public class House extends Zone {
         int currentLevel = getLevel();
         //if there are electrictiy, water and internet it can level up to level 1
         if (getElectricity() > 0 && getWater() > 0 && getInternet() > 0) {
-
-          //calculate the m value
+            //calculate the m value
             int m = getElectricity();
-            if(getWater()<m){
-                m = getWater();
-            }
-            if(getInternet()<m){
-                m = getInternet();
+            if(getWater() < m) m = getWater();
+            if(getInternet() < m) m = getInternet();
+
+            int targetLevel = 1;
+            //if there are security, health and education it can level up to level 2
+            if (getHasSecurity() && getHasHealth() && getHasEducation()) {
+                targetLevel = 2;
+                //if house get lifestyle it can level up to level 3
+                if (getLifestyle() > 0) {
+                    targetLevel = 3;
+                }
             }
 
-           if (currentLevel == 0) {//level up control
-                setLevel(1);
+            if (currentLevel < targetLevel) {
+                setLevel(currentLevel + 1);
+            } else if (currentLevel > targetLevel) {
+                setLevel(currentLevel - 1);
             }
-            else if (currentLevel == 1) {
-                if (getHasSecurity() == true && getHasHealth() == true && getHasEducation() == true) {//if there are security, health and education it can level up to level 2
-                    setLevel(2);
-                }
-            }
-            //if house get lifestyle it can level up to level 3
-            else if (currentLevel == 2) {
-                if (getLifestyle() > 0) {
-                    setLevel(3);
-                }
-            }
-            //logic of setOutput
-            if (getLevel()==1){
+            //logic of Output
+
+            if (getLevel() == 1) {
                 setOutput(m);
-            }else if (getLevel()==2){
-                setOutput(m*2);
-            } else if (getLevel()==3) {
-                setOutput((m*2)+getLifestyle());
+            } else if (getLevel() == 2) {
+                setOutput(m * 2);
+            } else if (getLevel() == 3) {
+                setOutput((m * 2) + getLifestyle());
+            } else {
+                setOutput(0);
             }
         } else {
-            // if basic needs not met zone collapses instantly
             setOutput(0);
             setLevel(0);
-            }
+        }
+        System.out.println("House at (" + getY() + "," + getX() + ") generated " + getOutput() + " population");
+        if (getLevel() > currentLevel) {
+            System.out.println("House at (" + getY() + "," + getX() + ") levels up from " + currentLevel + " to " + getLevel());
+        } else if (getLevel() < currentLevel) {
+            System.out.println("House at (" + getY() + "," + getX() + ") levels down from " + currentLevel + " to " + getLevel());
         }
     }
+}
